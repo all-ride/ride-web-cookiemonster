881bafba02e50bc975a111160112829201424c3f
Kaya <kayalion@kayalion.net>
2015-11-13 10:52:24 +0100

    added force flag

--- src/ride/web/CookieMonsterApplicationListener.php
+++ src/ride/web/CookieMonsterApplicationListener.php
@@ -5,6 +5,8 @@ namespace ride\web;
 use ride\library\event\Event;
 use ride\library\mvc\view\HtmlView;
 
+use ride\web\WebApplication;
+
 /**
  * Listener to hook the cookiemonster into Ride
  */
@@ -23,19 +25,49 @@ class CookieMonsterApplicationListener {
     const SCRIPT_TRANSLATOR = 'js/translator.js';
 
     /**
+     * Flag to see if the cookie monster should be added no matter what
+     * @var boolean
+     */
+    private $isForced;
+
+    /**
+     * Constructs the cookie monster
+     * @param boolean $isForced
+     * @return null
+     */
+    public function __construct($isForced = false) {
+        $this->isForced = $isForced;
+    }
+
+    /**
      * Event listener to add the cookiemonster to the response if applicable
-     * @param ride\library\mvc\Request $request
-     * @param ride\library\mvc\Response $response
+     * @param \ride\library\event\Event $event
+     * @return null
      */
     public function addCookieMonster(Event $event) {
         $web = $event->getArgument('web');
+        if ($this->shouldAddCookieMonster($web)) {
+            $view->addJavascript($request->getBaseUrl() . '/' . self::SCRIPT_TRANSLATOR);
+            $view->addJavascript($request->getBaseUrl() . '/' . self::SCRIPT_COOKIEMONSTER);
+        }
+    }
+
+    /**
+     * Checks if the cookie monster should be added
+     * @param \ride\web\WebApplication $web
+     * @return boolean
+     */
+    private function shouldAddCookieMonster(WebApplication $web) {
+        if ($this->isForced) {
+            return true;
+        }
 
         $request = $web->getRequest();
         $response = $web->getResponse();
 
         $view = $response->getView();
         if (!$view || !$view instanceof HtmlView) {
-            return;
+            return false;
         }
 
         $cookies = $response->getCookies();
@@ -48,8 +80,9 @@ class CookieMonsterApplicationListener {
         }
 
         if ($cookies) {
-            $view->addJavascript($request->getBaseUrl() . '/' . self::SCRIPT_TRANSLATOR);
-            $view->addJavascript($request->getBaseUrl() . '/' . self::SCRIPT_COOKIEMONSTER);
+            return true;
+        } else {
+            return false;
         }
     }
 
